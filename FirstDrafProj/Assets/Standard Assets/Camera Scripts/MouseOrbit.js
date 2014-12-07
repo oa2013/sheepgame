a99368c4efd14a11f26819c3b1699f5d47c0a72e
OA2013 <miohoya@gmail.com>
2014-12-06 22:30:50 -0500

    Mouse orbit horizontal changes
    Mouse follows girls movement but y-axis movement is not constrained

--- FirstDrafProj/Assets/Standard Assets/Camera Scripts/MouseOrbit.js
+++ FirstDrafProj/Assets/Standard Assets/Camera Scripts/MouseOrbit.js
@@ -1,4 +1,5 @@
 var target : Transform;
+
 var distance = 10.0;
 var distanceMin = 3;
 var distanceMax = 15;
@@ -6,11 +7,13 @@ var distanceMax = 15;
 var xSpeed = 250.0;
 var ySpeed = 120.0;
 
-var yMinLimit = -20;
-var yMaxLimit = 80;
+var yMinLimit = -20f;
+var yMaxLimit = 80f;
 
 private var x = 0.0;
 private var y = 0.0;
+
+var rotationY = 0;
 
 @AddComponentMenu("Camera-Control/Mouse Orbit")
 partial class MouseOrbit { }
@@ -23,29 +26,28 @@ function Start () {
 	// Make the rigid body not change rotation
    	if (rigidbody)
 		rigidbody.freezeRotation = true;
+		
 }
 
 function LateUpdate () {
     if (target) {
     
-        x += Input.GetAxis("Mouse X") * xSpeed * 0.02;
+        x += Input.GetAxis("Horizontal") * xSpeed * 0.02; //follow the player's horizontal rotation
         y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02;
  		
- 		y = ClampAngle(y, yMinLimit, yMaxLimit);
+ 		//y = ClampAngle(y, yMinLimit, yMaxLimit);
+		//Quaternion toRotation = Quaternion.Euler(x,y,0);
+		//Quaternion rotation = toRotation;
  		       
         var rotation = Quaternion.EulerAngles(y * Mathf.Deg2Rad, x * Mathf.Deg2Rad, 0);
         
-        distance = Mathf.Clamp(distance - Input.GetAxis("Mouse ScrollWheel")*5, distanceMin, distanceMax);
- 
-		var hit : RaycastHit;
-		if (Physics.Linecast (target.position, transform.position, hit)) {
-				distance -=  hit.distance;
-		}
-        
+        distance = Mathf.Clamp(distance - Input.GetAxis("Mouse ScrollWheel")*2, 0, 50);
+      
         var position = rotation * Vector3(0.0, 0.0, -distance) + target.position;
         
         transform.rotation = rotation;
         transform.position = position;
+       
     }
 }
 
