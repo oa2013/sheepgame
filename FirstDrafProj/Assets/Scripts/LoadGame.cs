b4e175c3af0d554fded020963c3f9942c917c84c
OA2013 <miohoya@gmail.com>
2014-12-08 20:20:30 -0500

    Menu start button works
    Yay

---
+++ FirstDrafProj/Assets/Scripts/LoadGame.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadGame : MonoBehaviour 
+{
+
+	void Start ()
+	{
+
+	}
+
+	void OnMouseOver()
+	{
+		if (GameObject.Find ("Start")) 
+		{
+			Application.LoadLevel("MainScene");
+		}
+	}
+
+}
